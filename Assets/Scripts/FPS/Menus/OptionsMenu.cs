67120a3d97b19ba1fba3fe59aeca38da1250743b
Rick Schlömilch <rschloem@hs-mittweida.de>
2023-08-13 15:30:42 +0200

    Options Menu Done

--- Assets/Scripts/FPS/Menus/OptionsMenu.cs
+++ Assets/Scripts/FPS/Menus/OptionsMenu.cs
@@ -1,13 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class OptionsMenu : MonoBehaviour
-{
-
-    public void SetQuality(int qualityIndex) {
-        QualitySettings.SetQualityLevel(qualityIndex);
-    }
+public class OptionsMenu : MonoBehaviour {
 
     public void SetFullscreen(bool isFullscreen) {
         Screen.fullScreen = isFullscreen;
